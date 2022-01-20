080cfb0246708552060607009cd7c672243ddbce
KonradMarzec1991 <konrad.marzec1991@gmail.com>
2022-01-20 12:08:00 +0100

    Remove date filters from ExpenseFilter class

--- server/budget/filters.py
+++ server/budget/filters.py
@@ -7,8 +7,12 @@ from .models import Budget, Expense
 class BaseFilter(filters.FilterSet):
     title = filters.CharFilter(method="filter_title")
 
-    created_after = filters.DateFilter(field_name="date_created", lookup_expr="gt")
-    created_before = filters.DateFilter(field_name="date_created", lookup_expr="lt")
+    created_after = filters.DateFilter(
+        field_name="date_created", lookup_expr="gt"
+    )
+    created_before = filters.DateFilter(
+        field_name="date_created", lookup_expr="lt"
+    )
 
     def filter_title(self, queryset, name, value):
         if value:
@@ -19,9 +23,6 @@ class BaseFilter(filters.FilterSet):
 class ExpenseFilter(BaseFilter):
     category = filters.CharFilter(method="filter_category")
     q = filters.CharFilter(method="filter_q")
-
-    created_after = filters.DateFilter(field_name="date_created", lookup_expr="gt")
-    created_before = filters.DateFilter(field_name="date_created", lookup_expr="lt")
 
     min_amount = filters.NumberFilter(field_name="amount", lookup_expr="gte")
     max_amount = filters.NumberFilter(field_name="amount", lookup_expr="lte")
@@ -37,7 +38,9 @@ class ExpenseFilter(BaseFilter):
 
     def filter_q(self, queryset, name, value):
         if value:
-            queryset = queryset.filter(Q(title__icontains=value) | Q(category__icontains=value))
+            queryset = queryset.filter(
+                Q(title__icontains=value) | Q(category__icontains=value)
+            )
         return queryset
 
 
